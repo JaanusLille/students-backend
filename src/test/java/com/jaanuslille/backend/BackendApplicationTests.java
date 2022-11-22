1c75f32c04a87fb424ab52a943b4f588e77506bf
Jaanus Lille <Jaanus.Lille@energia.ee>
2022-11-22 15:26:16 +0200

    Project ready

---
+++ src/test/java/com/jaanuslille/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaanuslille.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
